476ebc53066e546c55bfc2054a6e33991fd4970c
github username <github email address>
2021-04-25 11:14:06 -0400

    add name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
     has_many :trip_users
     has_many :trips, through: :trip_users
+    validates :name, :presence => true, :message => "Please include your name."
+    validates :email, :presence => true, :message => "Please enter your email."
+    validates :email, :uniqueness => true, :message => "Email already exists."
 
-    validates :email, :presence => true
-    validates :email, :unique => true
+
+
+
 end

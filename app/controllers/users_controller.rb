ea3f2f85d257e232d82722d5b77e440b8fe69403
github username <github email address>
2021-04-25 14:03:58 -0400

    add signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,18 @@
 class UsersController < ApplicationController
 
+
+  def new
+    @user = User.new
+  end 
+
+  def create
+
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   root 'welcome#home'
   get '/about', to: 'welcome#about'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
  
   get '/auth/facebook/callback' => 'sessions#create'
   get '/login' => 'sessions#new'

a5b9f2db1acee424f314e86ba134ece8ede26562
github username <github email address>
2021-04-25 10:42:11 -0400

    add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_many :trip_users
     has_many :trips, through: :trip_users
+
+    validates :email, :presence => true
+    validates :email, :unique => true
 end

8545b930fda5756856e87bf2a5a024e8b19af033
github username <github email address>
2021-04-26 12:41:05 -0400

    adjust scraper

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -47,7 +47,7 @@ class Scraper
         city = city_state[0].to_s.strip
         state = city_state[1].to_s.strip.upcase
         restaurant_name = eatery.css(".entry-title").css("a").text
-        food_categories = eatery.css(".rf-food-categories").css("a").text.split(/(?<=\p{Ll})(?=\p{Lu})|(?<=\p{Lu})(?=\p{Lu}\p{Ll})/)
+        food_categories = eatery.css(".rf-food-categories").css("a").text.split(/(?<=\p{Ll})(?=\p{Lu})|(?<=\p{Lu})(?=\p{Lu}\p{Ll})/).join(", ")
         dishes = eatery.css(".rf_dish_list").text.gsub("\n", " ").gsub("\t", " ").gsub(/ +?,/, ',').gsub(/\s+/, ' ')
         about = eatery.css(".rf-restaurant-preview").css("p").text
         dest[:city] = city

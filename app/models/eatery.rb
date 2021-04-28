66ec74914d8e0e4de60121df867cf5f76514c768
github username <github email address>
2021-04-28 12:55:35 -0400

    add header footer partials

--- app/models/eatery.rb
+++ app/models/eatery.rb
@@ -1,6 +1,13 @@
 class Eatery < ApplicationRecord
     has_many :eateries_trips                                         
-    has_many :trips, through: :eateries_trips    
+    has_many :trips, through: :eateries_trips   
+    
+    validates :city, :presence => true
+    validates :state, :presence => true
+    validates :name, :presence => true
+    validates :about, :presence => true
+    validates :food_categories, :presence => true
+    validates :dishes, :presence => true
 
     def location
         "#{self.city}, #{self.state}"

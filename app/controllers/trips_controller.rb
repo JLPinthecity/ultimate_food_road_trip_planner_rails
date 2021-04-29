2cf9c8ff078e98216bb87d729f5305c5cac44012
github username <github email address>
2021-04-28 23:26:11 -0400

    fix form

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,9 +3,15 @@ class TripsController < ApplicationController
     before_action :verify_user, only: [:new, :create, :edit, :update, :destroy]  #all users may see itineraries
 
     def new
-      find_user_by_id
-      @trip = Trip.new
+      # find_user_by_id
+      # @trip = Trip.new
+      # 5.times { @trip.eateries.build }
+      @trip = current_user.trips.build
       5.times { @trip.eateries.build }
+      @trip.eateries_trips.build
+      
+      
+
     end
    
     def create

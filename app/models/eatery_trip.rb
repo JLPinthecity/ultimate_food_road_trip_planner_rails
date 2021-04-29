d558a32d2cde0179774532a131cc57edd7ddeec5
github username <github email address>
2021-04-29 12:58:17 -0400

    refactor trip show page

--- app/models/eatery_trip.rb
+++ app/models/eatery_trip.rb
@@ -8,4 +8,12 @@ class EateryTrip < ApplicationRecord
 
     scope :visited, -> { where('visit_date IS NOT ? AND visit_date != ?', nil, '') }
     scope :reviewed, -> { where('reviewed IS NOT ? AND reviewed != ?', nil, '') }
+
+    def visit_date_exists
+      self.visit_date != "nil" || ""
+    end
+
+    def review_exists
+      self.eatery_trip.review != "nil" || "" 
+    end
 end

bf3f3042e6ebad26b273ee8bb64de846623f9b72
github username <github email address>
2021-05-05 16:15:43 -0400

    update trip new  with partials for readability

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -6,34 +6,37 @@ class Trip < ApplicationRecord
     validates :title, :presence => true
     validates :description, :presence => true
    
-    def self.create_from_collection(trips) #for scraper
-        u = User.find_by(:name => "Admin")
-        
-        trips.each do |trip_hash|  
-          binding.pry
-          trip = u.trips.create(:title => trip_hash[:title], :description => trip_hash[:description])
-            trip_hash[:eateries].each do |eatery|
-                a = trip.eateries.create(:name => eatery[:name], :city => eatery[:city], :state => eatery[:state], :food_categories => eatery[:food_categories],
-                                     :about => eatery[:about], :dishes => eatery[:dishes])
-            end      
-        end
-    end
+
 
     def eatery_trips_attributes=(attributes)
       attributes.values.each do |attribute|
+        #if attribute of eater_id is found/not blank, find create eatery_new with existing eatery
+        #else if att of eatery_id is not found/blank, create with custom eatery
 
-        #if 
+        
 
       end
 
 
     end
 
+    def self.create_from_collection(trips) #for scraper
+      u = User.find_by(:name => "Admin")
+      
+      trips.each do |trip_hash|  
+        binding.pry
+        trip = u.trips.create(:title => trip_hash[:title], :description => trip_hash[:description])
+          trip_hash[:eateries].each do |eatery|
+              a = trip.eateries.create(:name => eatery[:name], :city => eatery[:city], :state => eatery[:state], :food_categories => eatery[:food_categories],
+                                   :about => eatery[:about], :dishes => eatery[:dishes])
+          end      
+      end
+    end
 
-  # params.require(:trip).permit(:id, :title, :description, :eatery_trips_attributes => [:id, :visit_date, :review, :eatery_id, :eatery_attributes => [:id, :city, :state, :name, :about, :food_categories, :dishes]])
 
 end
 
+  # params.require(:trip).permit(:id, :title, :description, :eatery_trips_attributes => [:id, :visit_date, :review, :eatery_id, :eatery_attributes => [:id, :city, :state, :name, :about, :food_categories, :dishes]])
 
 
 
